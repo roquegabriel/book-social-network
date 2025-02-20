f630406c26cf02d48bbc8f3594e1e3094ca70d23
Roque <157759111+roquegabriel@users.noreply.github.com>
2025-02-20 14:41:59 -0300

    Protect routes with Auth Guard

--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
+  isTokenInvalid(): boolean {
+    return !this.isTokenValid();
+  }
+  isTokenValid(): boolean {
+    const token = this.token;
+    if (!token) {
+      return false;
+    }
+    //decode the token
+    const jwtHelper = new JwtHelperService();
 
-  set token(token: string){
+    //check expiry date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+
+    if (isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
+  }
+
+  set token(token: string) {
     localStorage.setItem('token', token);
   }
-  
-  get token() : string {
+
+  get token(): string {
     return localStorage.getItem('token') as string;
   }
-  
+
 }
